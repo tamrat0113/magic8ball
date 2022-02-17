a70e67f726e09601e4a85cdbcfc3425121d8a511
[tamrat] <tam980105@gmail.com>
2022-02-17 10:55:12 -0600

    update

--- js/magic8ball.js
+++ js/magic8ball.js
@@ -1,42 +1,38 @@
 //select the elements on the page to interact with
 const input = document.getElementById('Ask');
 const button = document.querySelector('button');
-
+const answer = document.querySelector('div');
 // Add a click event to the button
 button.addEventListener('click', ask);
 
 // Create a function that when clicked, it insert the magic8Ball into your "answers" div
-function ask(){
+function ask(e){
 // Find or select the child you want to remove, and use its parentNode property to find the parent:
-    const Ball = document.getElementById('bal')
+    const Ball = document.getElementById('bal');
     if(Ball != null){
         Ball.parentNode.removeChild(Ball);
     }
-    
+//Hide the inpute field when displaying the answer
+    input.style.display = 'none';
+//Add classlist on the button when clicked
     setTimeout(() => {
     button.classList.add('shaking');
     }, 1000);
 
     setTimeout(() =>{
-        button.classList.remove('shaking')
+        button.classList.remove('shaking');
     },3000);
+//excute the generatMagic8Bal function in two second
     setTimeout(() =>{
+        document.body.style.background="linear-gradient(90deg, #3e3e60, lightblue)";
         generatMagic8Ball();
-        label.style.display = 'block';
-        // input.style.background = "green";
+        input.replace(/<input[^>]*>|<\/input>/gi, "");
     },2000);  
-    // input.style.display ='none';
-    // input.value =true; 
-    
-    input.value ='';
 }
 
 // Generating a random Magic8Ball
 function generatMagic8Ball(){ 
-//selec the answer 'div';
-const answer = document.querySelector('div');
-
-//Creating an Array of magic8Ball
+//Creating an Array of magic8Ball using Array constractor
 let magicBall = new Array( "images/magic/magic8ball_10.png", "images/magic/magic8ball_11.png",
 "images/magic/magic8ball_9.png", "images/magic/magic8ball_13.png", "images/magic/magic8ball_14.png", "images/magic/magic8ball_4.png", "images/magic/magic8ball_6.png", "images/magic/magic8ball_7.png", "images/magic/magic8ball_2.png", "images/magic/magic8ball_1.png", "images/magic/magic8ball_3.png"); 
 
@@ -49,41 +45,3 @@ imageEl.setAttribute('id', 'bal');
 imageEl.src = `${randomM8Ball}`;
 answer.appendChild(imageEl);
 };
-
-
-
-
-
-
-
-
-
-// function ask(){
-    
-//     const Ball=document.getElementById('bal')
-//     console.log(Ball)
-//     if(Ball!=null){
-//         Ball.parentNode.removeChild(Ball);
-
-//     }
-
-//     // setTimeout(() => {
-//     //     images.classList.add('shaking');
-//     // }, 1000);
-
-//     // setTimeout(()=>{
-//     //     images.classList.remove('shaking')
-//     // },3000);
-    
-//     else{
-//         setTimeout(()=>{
-//             generatAnswer();
-//             // input.style.display='none';
-//            // input.value='visible';
-//         },2000); 
-//         // generatAnswer();
-         
-//     }
-//     // input.style.display='block'; 
-//     input.value='';
-// }
